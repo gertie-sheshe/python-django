43dd2dad879112294cc38bfc6ef410c0ad0716a0
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-06-27 15:46:07 +0300

    about and contact pages

--- templating-basics/mytemplate/website/views.py
+++ templating-basics/mytemplate/website/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class ContactUsPage(TemplateView):
+    template_name = 'contact.html'
