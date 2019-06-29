278651afa1ba16c75b61ea4b68145b36354cd55e
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-06-29 17:43:07 +0300

    setup database and mild interraction

--- commandr/cmdr/models.py
+++ commandr/cmdr/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Cmdr(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
--- commandr/cmdr/views.py
+++ commandr/cmdr/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Cmdr
 
 # Create your views here.
+class HomePageView(ListView):
+    model = Cmdr
+    template_name = 'home.html'
