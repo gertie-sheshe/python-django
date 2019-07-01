18a3810b48920f3536ab233535b236c4c48e8bad
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-07-01 21:14:00 +0300

    add views and make migration

--- forminput/videorequest/views.py
+++ forminput/videorequest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'videorequest/index.html')
+
+def requestform(request):
+    return render(request, 'videorequest/requestform.html')
