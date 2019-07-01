0e1cb073916fad99a90b20602a6eddea5492a2d2
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-07-01 15:18:42 +0300

    setup templating for articles view

--- simplesite/articles/views.py
+++ simplesite/articles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . models import Article
 
 # Create your views here.
+class ArticlesView(ListView):
+    model = Article
+    template_name = 'home.html'
