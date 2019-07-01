90a19b1727236dcbfee0a402f64f328bd2ef6852
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-07-01 19:28:36 +0300

    add unique url

--- simplesite/articles/urls.py
+++ simplesite/articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticlesView.as_view(), name='home')
+    path('', views.ArticlesView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_page')
 ]
--- simplesite/articles/views.py
+++ simplesite/articles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Article
 
 # Create your views here.
 class ArticlesView(ListView):
     model = Article
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'data_object'
