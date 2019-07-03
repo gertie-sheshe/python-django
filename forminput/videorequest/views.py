95eb08a613b32b5e5a94bb1c6d662741bad080c4
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-07-03 08:29:36 +0300

    add and save data with django form

--- forminput/videorequest/views.py
+++ forminput/videorequest/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Video
+from .forms import VideoForm
 
 # Create your views here.
 def index(request):
@@ -8,4 +9,20 @@ def index(request):
     return render(request, 'videorequest/index.html', context)
 
 def requestform(request):
-    return render(request, 'videorequest/requestform.html')
+    if request.method == 'POST':
+        form = VideoForm(request.POST)
+
+        if form.is_valid():
+            new_req = Video(
+                videotitle=request.POST['videoname'], 
+                videodesc=request.POST['videodesc'])
+            new_req.save()
+
+            return redirect('index')
+
+    else:
+        form = VideoForm()
+
+    context = {'form': form}
+
+    return render(request, 'videorequest/requestform.html', context)
