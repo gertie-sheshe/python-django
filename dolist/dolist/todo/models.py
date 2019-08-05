e31498f208b2ce5e72cc1e4bdae69c49dfca9a4f
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-08-05 12:00:15 +0300

    add model and register

--- dolist/dolist/todo/models.py
+++ dolist/dolist/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo(models.Model):
+    complete = models.BooleanField(default = False)
+    todoTask = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.todoTask
