957353ef584bfdf6e4b0b941aa1e514e504827d8
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-08-07 12:16:18 +0300

    add require_POST decorator

--- dolist/dolist/todo/views.py
+++ dolist/dolist/todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 from .forms import ToDoForm
 from .models import ToDo
@@ -10,4 +11,14 @@ def index(request):
     context = { 'mytodo': mytodo, 'form': form}
 
     return render(request, 'index.html', context)
+
+@require_POST
+def addNewToDo(request):
+    form = ToDoForm(request.POST)
+
+    if form.is_valid():
+        my_new_todo = ToDo(todotext=request.POST['text'])
+        my_new_todo.save()
+
+        return redirect('index')
 
